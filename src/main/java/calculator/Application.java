5de64e4e187bb2fc3e39a243c09e92a6b000a582
milk-stone <ywshero@naver.com>
2025-10-20 02:25:35 +0900

    feat(Application.java): 문자열 덧셈 계산기 구현 1단계 코드 작성
    - 테스트 `정상_기본_TC 1 ~ 5` 에 대한 테스트에 통과함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class Application {
+    private static final Pattern START_PATTERN = Pattern.compile("^\\/\\/\\W\\\\n");
+
     public static void main(String[] args) {
         System.out.println("사용자가 입력할 문자열을 입력해주세요.");
         String userInput = Console.readLine();
@@ -12,6 +19,11 @@ public class Application {
 
     public static int calculateSum(String userInput) {
         int sum = 0;
+        String numberPart = userInput;
+        sum = Arrays.stream(numberPart.split("[:,]"))
+                .filter(s -> !s.isEmpty())
+                .mapToInt(Integer::parseInt)
+                .sum();
         return sum;
     }
 }
