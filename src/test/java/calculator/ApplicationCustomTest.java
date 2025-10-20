b13bd6b8e0ebd8171dd35ec1ac204d18d8b93f8e
milk-stone <ywshero@naver.com>
2025-10-20 18:19:39 +0900

    test(ApplicationCustomTest.java): 테스트 코드 가독성 향상을 위헤 @ParameterizedTest 적용, 예외 상황에 대한 TC 추가

--- src/test/java/calculator/ApplicationCustomTest.java
+++ src/test/java/calculator/ApplicationCustomTest.java
@@ -1,98 +1,73 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ApplicationCustomTest extends NsTest {
-    @Test
-    void 정상_기본_TC1() {
+    @ParameterizedTest
+    @MethodSource("provideTestCasesWithoutCustomDelimiter")
+    void 정상_기본_테스트(String input, int expectedSum) {
         assertSimpleTest(() -> {
-            run("\n");
-            assertThat(output()).contains("결과 : 0");
+            run(input);
+            assertThat(output()).contains("결과 : " + expectedSum);
         });
     }
 
-    @Test
-    void 정상_기본_TC2() {
+    private static Stream<Arguments> provideTestCasesWithoutCustomDelimiter() {
+        return Stream.of(
+                Arguments.of("\n", 0),
+                Arguments.of("3", 3),
+                Arguments.of("1,2", 3),
+                Arguments.of("1:2", 3),
+                Arguments.of("1:2,3", 6)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("provideTestCasesWithCustomDelimiter")
+    void 정상_커스텀_테스트(String input, int expectedSum) {
         assertSimpleTest(() -> {
-            run("3");
-            assertThat(output()).contains("결과 : 3");
+            run(input);
+            assertThat(output()).contains("결과 : " + expectedSum);
         });
     }
 
-    @Test
-    void 정상_기본_TC3() {
-        assertSimpleTest(() -> {
-            run("1,2");
-            assertThat(output()).contains("결과 : 3");
-        });
+    private static Stream<Arguments> provideTestCasesWithCustomDelimiter() {
+        return Stream.of(
+                Arguments.of("//;\\n", 0),
+                Arguments.of("//;\\n1", 1),
+                Arguments.of("//;\\n1,2", 3),
+                Arguments.of("//;\\n1:2", 3),
+                Arguments.of("//;\\n1;2", 3),
+                Arguments.of("//;\\n1:2;3", 6)
+        );
     }
 
-    @Test
-    void 정상_기본_TC4() {
-        assertSimpleTest(() -> {
-            run("1:2");
-            assertThat(output()).contains("결과 : 3");
-        });
+    @ParameterizedTest
+    @MethodSource("provideInvalidInputs")
+    void 예외처리_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
-    @Test
-    void 정상_기본_TC5() {
-        assertSimpleTest(() -> {
-            run("1:2,3");
-            assertThat(output()).contains("결과 : 6");
-        });
-    }
-
-    @Test
-    void 정상_커스텀_TC1() {
-        assertSimpleTest(() -> {
-            run("//;\\n");
-            assertThat(output()).contains("결과 : 0");
-        });
-    }
-
-    @Test
-    void 정상_커스텀_TC2() {
-        assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
-        });
-    }
-
-    @Test
-    void 정상_커스텀_TC3() {
-        assertSimpleTest(() -> {
-            run("//;\\n1,2");
-            assertThat(output()).contains("결과 : 3");
-        });
-    }
-
-    @Test
-    void 정상_커스텀_TC4() {
-        assertSimpleTest(() -> {
-            run("//;\\n1:2");
-            assertThat(output()).contains("결과 : 3");
-        });
-    }
-
-    @Test
-    void 정상_커스텀_TC5() {
-        assertSimpleTest(() -> {
-            run("//;\\n1;2");
-            assertThat(output()).contains("결과 : 3");
-        });
-    }
-
-    @Test
-    void 정상_커스텀_TC6() {
-        assertSimpleTest(() -> {
-            run("//;\\n1:2;3");
-            assertThat(output()).contains("결과 : 6");
-        });
+    private static Stream<Arguments> provideInvalidInputs() {
+        return Stream.of(
+                Arguments.of("-1,2,3"),
+                Arguments.of("//;\\n1;-2"),
+                Arguments.of("//A\\n1A2"),
+                Arguments.of("1,a,2")
+        );
     }
 
     @Override
